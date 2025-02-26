b8ca5b7debaa2c1d4ed51a2ac2be8c2a151a5d7b
Vladimir Buskin <sla1031@ua-212-mac6.ku.de>
2025-02-26 18:27:27 +0100

    full clean render

--- docs/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const headers = document.querySelectorAll('.sidebar-header');
-    headers.forEach(header => {
-        header.addEventListener('click', function() {
-            const section = header.nextElementSibling;
-            section.classList.toggle('collapsed');
-        });
-    });
-});
